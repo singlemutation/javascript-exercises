dd20c4a07ca46d3947e8293dd3642141b464dd3d
singlemutation <164826575+singlemutation@users.noreply.github.com>
2024-05-08 17:49:08 -0400

    Solve fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = parseInt(num);
+    if (num === 0) {
+      return 0;
+    };
+    if (num < 0) {
+      return 'OOPS';
+    }
+    let fib = [];
+    for (let i = 0; i < num; i++) {
+      if ((i === 0) || (i === 1)) {
+        fib.push(1);
+      } else {
+        fib.push((fib[i - 1] + fib[i - 2]))
+      }
+    }
+    return fib[fib.length - 1]
 };
 
 // Do not edit below this line
