ff2678f30f6b63045cd85e0fa1144b2555ffec9c
singlemutation <164826575+singlemutation@users.noreply.github.com>
2024-05-08 20:02:43 -0400

    Solve findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, currentPerson) => {
+        let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        let currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestAge < currentAge ? currentPerson : oldest
+    });
+};
 
+function getAge(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
 };
 
 // Do not edit below this line
